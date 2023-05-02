7d40e621e50be6885d4e09851fd90eb2a3682e69
dg1223 <alavi1223@hotmail.com>
2023-05-02 00:57:40 -0400

    rightnav complete

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import {
+  FaBeer,
+  FaGithub,
+  FaGoogle,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
+import QZone from "../QZone/QZone";
+import bg from "../../../assets/bg.png";
+import bg1 from "../../../assets/bg1.png";
 
 const RightNav = () => {
   return (
     <div>
-      <h2>Right Nav</h2>
+      <h4>Login with</h4>
+      <Button variant="outline-primary">
+        <FaGoogle /> Login with Google
+      </Button>
+      <Button variant="outline-secondary">
+        {" "}
+        <FaGithub />
+        Login with GitHub
+      </Button>
+      <div>
+        <h4>Find us on</h4>
+        <ListGroup>
+          <ListGroup.Item>
+            <FaFacebook />
+            Facebook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaTwitter />
+            Twitter
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaInstagram />
+            Instagram
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone></QZone>
+      <div></div>
     </div>
   );
 };
